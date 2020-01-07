4fa679c72ee90d99b8f91bcc1ee66e8ccadd3f3b
Evan Nemetz <37675159+enemetz@users.noreply.github.com>
2020-01-07 01:47:52 -0500

    Add files via upload

--- HueDeck.py
+++ HueDeck.py
@@ -1,1 +1,55 @@
-print("Hello")
+from phue import Bridge
+import eel
+
+# Eel using front end directory
+eel.init('web')
+
+b = Bridge('192.168.1.8')
+# Have to hit button on bridge and then run b.connect only once
+# b.connect()
+b.get_api()
+
+
+# When light x is pressed, will turn on if off, will turn off if on
+@eel.expose
+def lightSwitchOnePressed():
+    if b.get_light(1, 'on'):
+        b.set_light(1, 'on', False)
+    else:
+        b.set_light(1, 'on', True)
+
+
+@eel.expose
+def lightSwitchTwoPressed():
+    if b.get_light(2, 'on'):
+        b.set_light(2, 'on', False)
+    else:
+        b.set_light(2, 'on', True)
+
+
+@eel.expose
+def lightSwitchThreePressed():
+    if b.get_light(3, 'on'):
+        b.set_light(3, 'on', False)
+    else:
+        b.set_light(3, 'on', True)
+
+
+@eel.expose
+def lightSwitchFourPressed():
+    if b.get_light(4, 'on'):
+        b.set_light(4, 'on', False)
+    else:
+        b.set_light(4, 'on', True)
+
+
+@eel.expose
+def isOneOn():
+    print("Made it")
+    if b.get_light(1, 'on'):
+        return "On"
+    else:
+        return False
+
+
+eel.start('HueDeckUI.html', size=(800, 480))
